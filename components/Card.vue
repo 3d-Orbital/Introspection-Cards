0d40ab7e7af9b163f9412a87224f9751f7242989
esviren <st-rodriguez@hotmail.com>
2024-05-02 16:53:10 -0500

    add changes to cards.json

--- components/Card.vue
+++ components/Card.vue
@@ -1,23 +1,29 @@
 <template>
-    <UCard class="card mx-8" style="width: 20rem;">
+    <UCard class="card mx-2 my-8 w-80 min-w-80 ring-{{borderColor}}-200 dark:ring-{{borderColor}}-800" style="width: 20rem;" >
         <template #header>
             <h2 class="uppercase">{{ title }}</h2>
-            <Placeholder class="h-8" />
+            <Placeholder class="h-10" />
         </template>
         <Placeholder class="h-32" />
+        <img slot="image" class="card-img-top pb-2" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22320%22%20height%3D%22180%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20320%20180%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_1664f761b63%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A16pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_1664f761b63%22%3E%3Crect%20width%3D%22320%22%20height%3D%22180%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22110.203125%22%20y%3D%2297.35%22%3EImage%20cap%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" alt="Card image cap">
+        <p class="card-text">{{ content }} </p>
+        <p class="card-text py-2" >{{ section }}</p>
         <template #footer>
             <Placeholder class="h-8" />
+            <p>{{ footer }}</p>
         </template>
-        <img slot="image" class="card-img-top" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22320%22%20height%3D%22180%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20320%20180%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_1664f761b63%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A16pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_1664f761b63%22%3E%3Crect%20width%3D%22320%22%20height%3D%22180%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22110.203125%22%20y%3D%2297.35%22%3EImage%20cap%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" alt="Card image cap">
-        <p class="card-text">{{ content }} Some quick example text to build on the card content.</p>
+       
     </UCard>
 </template>
 
 <script setup lang="ts">
+import { _borderColor } from '#tailwind-config/theme';
+
 defineProps({
     id: Number, 
     title: String,
-    content: String, 
+    content: String,
+    section: String,
     footer: String,
     cardType: String,
     borderColor: String,
@@ -27,5 +33,12 @@ defineProps({
     imageBack: String,
     value: String
 })
+
+const toggle = () => {
+    
+}
+
+const color_border = _borderColor;
+
 </script>
 
